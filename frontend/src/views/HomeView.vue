33ac33f280c9623bc9b8fb9f653f56a58a371d75
Max Ruffo <Max.Ruffo@Student.HTW-Berlin.de>
2023-05-08 11:49:36 +0200

    Frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
     <div className="home">
         <h1>Welcome to Habitrackus</h1>
+        <p className = "text1" >Enigma Chatroom is a secure messaging website that uses advanced encryption techniques to protect your messages from prying eyes. The website employs the SHA256 algorithm and an Enigma machine to encrypt your messages, ensuring that only the intended recipient can read them. With Enigma Chatroom, you can communicate with confidence, knowing that your messages are safe and secure. Whether you're discussing sensitive business information or simply chatting with friends and family, Enigma Chatroom provides the peace of mind you need to communicate freely and securely. Join the Enigma Chatroom community today and experience the power of secure messaging for yourself.</p>
         <br/><br/>
         <div className="container text-center">
             <div className="row">
@@ -25,6 +26,16 @@ nav {
     font-family: 'Muli', sans-serif;
     color: white;
 }
+
+home {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    gap: 2rem;
+}
+
 </style>
 
 
